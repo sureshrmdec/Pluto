c1d89f994d68d413dabdb390045a339700d3c26c
Jeffwan <seedjeffwan@gmail.com>
2015-02-02 15:18:11 -0800

    Pointeraces added

---
+++ src/com/jiaxin/company/linkedin/Pointacres.java
@@ -0,0 +1,14 @@
+package com.jiaxin.company.linkedin;
+
+public class Pointacres {
+	
+	//http://www.1point3acres.com/bbs/forum.php?mod=viewthread&tid=115893&extra=page%3D1%26filter%3Dsortid%26sortid%3D311%26searchoption%5B3046%5D%5Bvalue%5D%5B6%5D%3D6%26searchoption%5B3046%5D%5Btype%5D%3Dcheckbox%26sortid%3D311
+	//1.http://www.geeksforgeeks.org/equilibrium-index-of-an-array/
+	
+	//2. http://www.geeksforgeeks.org/a-product-array-puzzle/
+	
+	//3.Find the shortest distance between two words in a string
+
+	
+	//http://www.1point3acres.com/bbs/thread-105717-1-1.html
+}
