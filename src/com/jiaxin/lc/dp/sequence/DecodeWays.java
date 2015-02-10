af70c7df298011b6d4db265964acf332e0399e6d
Jeffwan <seedjeffwan@gmail.com>
2015-02-09 18:20:38 -0800

    update Feb 9

--- src/com/jiaxin/lc/dp/sequence/DecodeWays.java
+++ src/com/jiaxin/lc/dp/sequence/DecodeWays.java
@@ -1,5 +1,7 @@
 package com.jiaxin.lc.dp.sequence;
 
+import org.junit.Test;
+
 
 public class DecodeWays {
 	// Test case: "10", "100"  if 0 apears and can't form a two digits. it will be 0. can't be decoded.
@@ -29,4 +31,33 @@ public class DecodeWays {
 	
 	// Recursive http://stackoverflow.com/questions/20342462/review-an-answer-decode-ways
 	// https://gist.github.com/janehwzn/5937872
+	
+	public int numDecodingsRecursive(String s) {
+		if (s == null || s.length() == 0) {
+			return 0;
+		}
+		
+		if (s.length() == 1) {
+			return s.charAt(0) == '0' ? 0 : 1; 
+		} 
+		
+		int first = s.charAt(0) - '0';
+		int second = s.charAt(1) - '0';
+		int twoDigit = first * 10 + second;
+		
+		if (twoDigit >= 10 && twoDigit <= 26) {
+			return numDecodingsRecursive(s.substring(1)) + numDecodingsRecursive(s.substring(2));
+		} else {
+			return numDecodingsRecursive(s.substring(1));
+		}
+	}
+	
+	
+	@Test
+	public void test() {
+		String s = "1584232141014";
+		
+		System.out.println(numDecodings(s));
+		System.out.println(numDecodingsRecursive(s));
+	}
 }
